768dd64ca6f0e30fe088b81a03af05c0070e3e1a
Adnan Ashraf <adnanashraf4423@gmail.com>
2024-03-23 23:51:04 +0500

    fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-	plugins: {
-		"postcss-nested": {}, // Add this line
-		tailwindcss: {},
-		autoprefixer: {},
-	},
+  plugins: [
+    "postcss-import",
+    "tailwindcss/nesting",
+	"postcss-nesting",
+    "autoprefixer",
+    "tailwindcss",
+  ],
 };
