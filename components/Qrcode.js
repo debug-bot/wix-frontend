b6421efb219a70f009e553a1166ff6c14cc28db0
Adnan Ashraf <adnanashraf4423@gmail.com>
2024-03-24 22:37:17 +0500

    fixed

--- components/Qrcode.js
+++ components/Qrcode.js
@@ -5,94 +5,102 @@ import AuthContext from "../context/AuthContext";
 const swal = require("sweetalert2");
 
 export default function Qrcode() {
-	const [url, setUrl] = useState("");
-	const [qrurl, setQrUrl] = useState("");
-	const [generate, setGenerate] = useState(false);
-	const { user } = useContext(AuthContext);
+  const [url, setUrl] = useState("");
+  const [name, setName] = useState(""); // New state for QR code name
+  const [generate, setGenerate] = useState(false);
+  const { user } = useContext(AuthContext);
 
-	const user_id = user?.user_id || 1;
+  const user_id = user?.user_id || 1;
 
-	const handleGenerateQR = (e) => {
-		e.preventDefault();
-		if (url) {
-			setGenerate(true);
-			setQrUrl(url);
-			try {
-				const response = axios.post(
-					"http://127.0.0.1:8000/store/qrcode-history/",
-					{
-						user: user_id,
-						url: url,
-					}
-				);
-				console.log(response.data);
-				swal.fire({
-					title: "Your QR code saved successfully!",
-					icon: "success",
-					toast: true,
-					timer: 3000,
-					position: "bottom-right",
-					timerProgressBar: true,
-					showConfirmButton: false,
-				});
-			} catch (error) {
-				console.error("There was an error!", error);
-				swal.fire({
-					title: "Something went wrong!",
-					icon: "error",
-					toast: true,
-					timer: 3000,
-					position: "bottom-right",
-					timerProgressBar: true,
-					showConfirmButton: false,
-				});
-			}
-		}
-	};
+  const handleGenerateQR = async (e) => {
+    e.preventDefault();
+    if (url) {
+      setGenerate(true);
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/store/qrcode-history/",
+          {
+            user: user_id,
+            url: url,
+            name: name, // Sending the QR code name to the server
+          }
+        );
+        console.log(response.data);
+        swal.fire({
+          title: "Your QR code saved successfully!",
+          icon: "success",
+          toast: true,
+          timer: 3000,
+          position: "bottom-right",
+          timerProgressBar: true,
+          showConfirmButton: false,
+        });
+      } catch (error) {
+        console.error("There was an error!", error);
+        swal.fire({
+          title: "Something went wrong!",
+          icon: "error",
+          toast: true,
+          timer: 3000,
+          position: "bottom-right",
+          timerProgressBar: true,
+          showConfirmButton: false,
+        });
+      }
+    }
+  };
 
-	return (
-		<>
-			<div className="container" style={{ marginBottom: "200px" }}>
-				<h1 className="text-center my-5">QR Code Generator</h1>
-				<div className="row justify-content-center">
-					<div className="col-md-6">
-						<form onSubmit={handleGenerateQR}>
-							<div className="input-group mb-3">
-								<input
-									type="url"
-									className="form-control"
-									placeholder="Enter URL"
-									value={url}
-									onChange={(e) => setUrl(e.target.value)}
-									required
-								/>
-								<button className="btn btn-light" type="submit">
-									Generate QR Code
-								</button>
-							</div>
-						</form>
-						{generate ? (
-							<div className="d-flex flex-column justify-content-center align-items-center">
-								<p>
-									<strong>Your link: </strong>
-									<a href={qrurl} target="_blank">
-										{qrurl}
-									</a>
-								</p>
-								<div className="text-center p-3 bg-light rounded">
-									<QRCode value={qrurl} renderAs="canvas" />
-								</div>
-							</div>
-						) : (
-							<>
-								<div className="text-center pt-3">
-									<p>Enter URL to generate QR code</p>
-								</div>
-							</>
-						)}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container" style={{ marginBottom: "200px" }}>
+        <h1 className="text-center my-5">QR Code Generator</h1>
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <form onSubmit={handleGenerateQR}>
+              <div className="input-group mb-3">
+                <input
+                  type="url"
+                  className="form-control"
+                  placeholder="Enter URL"
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter QR Code Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <button className="btn btn-dark" type="submit">
+                Save QR Code
+              </button>
+            </form>
+            {generate && (
+              <div className="d-flex flex-column justify-content-center align-items-center mt-4">
+                <p>
+                  <strong>QR Code link: </strong>
+                  <a href={url} target="_blank">
+                    {url}
+                  </a>
+                </p>
+                <p>
+                  <strong>QR Code Name: </strong>
+                  {name}
+                </p>
+                <div className="text-center p-3 bg-light rounded">
+                  <QRCode value={url} renderAs="canvas" />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- components/QrcodeHistory.js
+++ components/QrcodeHistory.js
@@ -5,108 +5,144 @@ import AuthContext from "../context/AuthContext";
 const swal = require("sweetalert2");
 
 export default function QrcodeHistory() {
-	const [history, setHistory] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState("");
-	const { user } = useContext(AuthContext);
-	const userId = user?.user_id || 1;
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { user } = useContext(AuthContext);
+  const userId = user?.user_id || 1;
 
-	useEffect(() => {
-		fetchQRCodeHistory();
-	}, []);
+  useEffect(() => {
+    fetchQRCodeHistory();
+  }, []);
 
-	const fetchQRCodeHistory = async () => {
-		setLoading(true);
-		try {
-			const response = await axios.get(
-				`http://127.0.0.1:8000/store/qrcode-history/?user_id=${userId}`
-			);
-			setHistory(response.data);
-		} catch (error) {
-			setError("Error fetching QR code history");
-			console.error("Error fetching QR code history:", error);
-		} finally {
-			setLoading(false);
-		}
-	};
+  const fetchQRCodeHistory = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:8000/store/qrcode-history/?user_id=${userId}`
+      );
+      setHistory(response.data);
+    } catch (error) {
+      setError("Error fetching QR code history");
+      console.error("Error fetching QR code history:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	const handleDelete = async (id) => {
-		try {
-			const confirmed = await swal.fire({
-				title: "Are you sure?",
-				text: "This action cannot be undone.",
-				icon: "warning",
-				showCancelButton: true,
-				confirmButtonColor: "#3085d6",
-				cancelButtonColor: "#d33",
-				confirmButtonText: "Yes, delete it!",
-			});
+  const handleDelete = async (id) => {
+    try {
+      const confirmed = await swal.fire({
+        title: "Are you sure?",
+        text: "This action cannot be undone.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      });
 
-			if (confirmed.isConfirmed) {
-				const response = await axios.delete(
-					`http://127.0.0.1:8000/store/qrcode-history/?user_id=${userId}&qrcode_id=${id}`
-				);
-				setHistory(history.filter((item) => item.id !== id));
-				swal.fire("Deleted!", response.data.message, "success");
-			}
-		} catch (error) {
-			setError("Error deleting QR code");
-			console.error("Error deleting QR code:", error);
-		}
-	};
+      if (confirmed.isConfirmed) {
+        const response = await axios.delete(
+          `http://127.0.0.1:8000/store/qrcode-history/?user_id=${userId}&qrcode_id=${id}`
+        );
+        setHistory(history.filter((item) => item.id !== id));
+        swal.fire("Deleted!", response.data.message, "success");
+      }
+    } catch (error) {
+      setError("Error deleting QR code");
+      console.error("Error deleting QR code:", error);
+    }
+  };
 
-	return (
-		<div>
-			<h1 className="text-center my-5">QR Code Generation History</h1>
-			{loading ? (
-				<p>Loading...</p>
-			) : error ? (
-				<p>{error}</p>
-			) : history.length > 0 ? (
-				<table className="table table-striped table-bordered">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>URL</th>
-							<th>Qrcode</th>
-							<th>Created</th>
-							<th>Delete</th>
-						</tr>
-					</thead>
-					<tbody>
-						{history.map((record, index) => (
-							<tr key={record.id}>
-								<th>{index + 1}</th>
-								<td>
-									<a
-										href={record.url}
-										target="_blank"
-										rel="noreferrer"
-									>
-										{record.url}
-									</a>
-								</td>
-								<td>
-									<div className="d-flex justify-content-center">
-										<QRCode value={record.url} />
-									</div>
-								</td>
-								<td>{record.time_since_updated} ago</td>
-								<td>
-									<button
-										className="btn btn-danger"
-										onClick={() => handleDelete(record.id)}
-									>
-										Delete
-									</button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			) : (
-				<p className="text-center">No QR code history available.</p>
-			)}
-		</div>
-	);
+  const handleDownloadQRCode = (name, id) => {
+    // Find the canvas element
+    const canvas = document.getElementById(`qrcode-canvas-${id}`);
+    if (canvas) {
+      const pngUrl = canvas.toDataURL("image/png");
+      let downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = `${name}.png`;
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    }
+  };
+
+  const truncateURL = (url, maxLength = 40) => {
+    return url.length > maxLength ? url.substring(0, maxLength) + "..." : url;
+  };
+
+  return (
+    <div>
+      <h1 className="text-center my-5">QR Code Generation History</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : history.length > 0 ? (
+        <div className="table-responsive">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Id</th>
+                <th>Name</th>
+                <th>URL</th>
+                <th>Qrcode</th>
+                <th>Created</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {history.map((record, index) => (
+                <tr key={record.id}>
+                  <th>{index + 1}</th>
+                  <th>{record.id}</th>
+                  <td>{record.name}</td>
+                  <td>
+                    <a
+                      href={record.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-truncate"
+                    >
+                      {truncateURL(record.url)}
+                    </a>
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-center">
+                      <QRCode
+                        value={record.url}
+                        id={`qrcode-canvas-${record.id}`}
+                      />
+                    </div>
+                  </td>
+                  <td>{record.time_since_updated} ago</td>
+                  <td>
+                    <button
+                      className="btn btn-dark me-3"
+                      onClick={() =>
+                        handleDownloadQRCode(record.name, record.id)
+                      }
+                    >
+                      Download
+                    </button>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(record.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <p className="text-center">No QR code history available.</p>
+      )}
+    </div>
+  );
 }
